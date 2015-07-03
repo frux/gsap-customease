ff8a5374a6b2aba0838492d79284ec1143963068
Vladimir Kudinov <frux@yandex-team.ru>
2015-07-03 18:47:37 +0300

    Init

---
+++ customease.js
@@ -0,0 +1,34 @@
+var CustomEase = (function(){
+    var easings = {};
+
+    function create(name, points){
+        var sections = points.length,
+            sectionStep = 1 / sections,
+            curves = [],
+            i;
+
+        for(i = 0; i < sections; i++){
+            curves.push((function(p){
+                return function(t){
+                    return Math.pow(1 - t, 2) * p.s + 2 * t * (1 - t) * p.cp + Math.pow(t, 2) * p.e;
+                }
+            })(points[i]));
+        }
+
+        easings[name] = function(t){
+            var curveIndex = Math.floor(t / sectionStep),
+                curveProgress = t % sectionStep / sectionStep;
+
+            return curves[curveIndex](curveProgress);
+        };
+    }
+
+    function byName(name){
+        return easings[name];
+    }
+
+    return {
+        create: create,
+        byName: byName
+    };
+})();
